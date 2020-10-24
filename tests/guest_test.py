0d2c3fcb733bac7da4ce74f47ea5ffd8ffbe65e2
Michael <admin@fillthegaplearning.co.uk>
2020-10-24 12:41:19 +0100

    Added additional guests

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,7 +5,10 @@ from src.guest import Guest
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("John")
+        self.guest1 = Guest("John")
+        self.guest2 = Guest("Sally")
+        self.guest3 = Guest("Bill")
+        self.guest4 = Guest("Wendy")
 
-    def test_guest_is_created(self):
+    def test_guest_has_name(self):
         self.assertEqual(self.guest.first_name, self.guest.create_guest())
